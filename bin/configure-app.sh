d6cf00c0304b18ff9d64baad066be5bebb9f5215
Seshadri Mahalingam <sesh@trifacta.com>
2018-06-04 15:42:29 -0700

    Change default key vault address to a valid URL
    Summary: Allows secure-token-service to start up gracefully.
    
    Test Plan: Deploy to an HDInsight cluster.
    
    Reviewers: tan, kgandhi, jaypranavamurthi
    
    Reviewed By: jaypranavamurthi
    
    Subscribers: vineet
    
    JIRA Issues: TD-29822
    
    Differential Revision: https://phab.trifacta.com/D28716

--- bin/configure-app.sh
+++ bin/configure-app.sh
@@ -440,7 +440,7 @@ function ConfigureEdgeNode() {
 function StartTrifacta() {
   LogInfo "Starting Trifacta"
   chmod 666 "$triconf"
-  service trifacta restart
+  service trifacta restart || true
 }
 
 function CreateHiveConnection() {
